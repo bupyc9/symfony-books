08d0795f62b9544b5b577255d8668ddfc5a71d65
Pavel Afanasev <bupyc9@gmail.com>
2019-02-27 23:36:35 +0300

    Edit author form

--- source/src/Controller/AuthorController.php
+++ source/src/Controller/AuthorController.php
@@ -3,15 +3,35 @@
 namespace App\Controller;
 
 use App\Entity\Author;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class AuthorController extends AbstractController
 {
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     * @return AuthorController
+     *
+     * @required
+     */
+    public function setTranslator(TranslatorInterface $translator): self
+    {
+        $this->translator = $translator;
+
+        return $this;
+    }
+
     /**
      * @Route("/authors", name="authors", methods={"GET", "HEAD"})
      * @throws \LogicException
@@ -24,13 +44,12 @@ class AuthorController extends AbstractController
         return $this->render('author/index.html.twig', ['authors' => $authors]);
     }
 
-
     /**
-     * @Route("/authors/create", name="author_create", methods={"GET", "POST", "HEAD"})
-     *
      * @param Request $request
      * @return Response
      * @throws \LogicException
+     *
+     * @Route("/authors/create", name="author_create", methods={"GET", "HEAD", "POST"})
      */
     public function create(Request $request): Response
     {
@@ -40,7 +59,7 @@ class AuthorController extends AbstractController
             ->add('firstName', TextType::class, ['label' => 'author.firstName', 'attr' => ['maxlength' => 255]])
             ->add('lastName', TextType::class, ['label' => 'author.lastName', 'attr' => ['maxlength' => 255]])
             ->add('secondName', TextType::class, ['label' => 'author.secondName', 'required' => false])
-            ->add('save', SubmitType::class, ['label' => 'Добавить'])
+            ->add('save', SubmitType::class, ['label' => $this->translator->trans('base.add')])
             ->getForm()
         ;
 
@@ -56,5 +75,38 @@ class AuthorController extends AbstractController
         }
 
         return $this->render('author/create.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @param Author $author
+     * @param Request $request
+     * @return Response
+     * @throws \LogicException
+     *
+     * @ParamConverter(class="App\Entity\Author")
+     * @Route("/authors/{id<\d+>}", name="author_edit", methods={"GET", "HEAD", "POST"})
+     */
+    public function edit(Author $author, Request $request): Response
+    {
+        $form = $this->createFormBuilder($author)
+            ->add('firstName', TextType::class, ['label' => 'author.firstName', 'attr' => ['maxlength' => 255]])
+            ->add('lastName', TextType::class, ['label' => 'author.lastName', 'attr' => ['maxlength' => 255]])
+            ->add('secondName', TextType::class, ['label' => 'author.secondName', 'required' => false])
+            ->add('save', SubmitType::class, ['label' => $this->translator->trans('base.edit')])
+            ->getForm()
+        ;
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $author = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($author);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('authors');
+        }
+
+        return $this->render('author/edit.html.twig', ['form' => $form->createView(), 'author' => $author]);
     }
 }
