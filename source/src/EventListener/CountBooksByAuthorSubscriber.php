b2e307b729ea9b3c5bc4039290a82bc3aeef7e6a
Pavel Afanasev <bupyc9@gmail.com>
2019-03-01 00:16:57 +0300

    Count books by author subscriber

---
+++ source/src/EventListener/CountBooksByAuthorSubscriber.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Entity\Book;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
+use Doctrine\ORM\Events;
+
+class CountBooksByAuthorSubscriber implements EventSubscriber
+{
+    public function getSubscribedEvents(): array
+    {
+        return [
+            Events::postPersist,
+            Events::preUpdate,
+            Events::postUpdate,
+            Events::postRemove,
+        ];
+    }
+
+    public function postPersist(LifecycleEventArgs $event): void
+    {
+        $this->updateCount($event);
+    }
+
+    public function preUpdate(PreUpdateEventArgs $event): void
+    {
+        $entity = $event->getObject();
+
+        if (!$entity instanceof Book) {
+            return;
+        }
+
+        if (!$event->hasChangedField('author')) {
+            return;
+        }
+
+        $entityManager = $event->getObjectManager();
+        $author = $event->getOldValue('author');
+        $count = $entityManager->getRepository(Book::class)->count(['author' => $author]);
+        $author->setCountBooks(--$count);
+        $entityManager->persist($author);
+    }
+
+    public function postUpdate(LifecycleEventArgs $event): void
+    {
+        $this->updateCount($event);
+    }
+
+    public function postRemove(LifecycleEventArgs $event): void
+    {
+        $this->updateCount($event);
+    }
+
+    private function updateCount(LifecycleEventArgs $event): void
+    {
+        $entity = $event->getObject();
+
+        if (!$entity instanceof Book) {
+            return;
+        }
+
+        $author = $entity->getAuthor();
+
+        if (null === $author) {
+            return;
+        }
+
+        $entityManager = $event->getObjectManager();
+        $count = $entityManager->getRepository(Book::class)->count(['author' => $author]);
+        $author->setCountBooks($count);
+        $entityManager->persist($author);
+        $entityManager->flush();
+    }
+}
