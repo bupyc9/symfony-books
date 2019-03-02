3e5f430cdb4e7e3abfe94538e6b1ecdc268fdae2
Pavel Afanasev <bupyc9@gmail.com>
2019-03-02 23:53:03 +0300

    Display of the list on the main page in the form of columns

--- source/src/Controller/HomeController.php
+++ source/src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    private const ITEMS_ON_PAGE = 10;
+    private const ITEMS_ON_PAGE = 9;
 
     /**
      * @var PaginatorInterface
