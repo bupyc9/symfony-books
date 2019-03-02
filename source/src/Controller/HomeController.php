8a9295b1b6a977ab3a443de296557c9af92d5445
Pavel Afanasev <bupyc9@gmail.com>
2019-03-02 23:44:32 +0300

    Added pagination for home lists

--- source/src/Controller/HomeController.php
+++ source/src/Controller/HomeController.php
@@ -3,21 +3,46 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+    private const ITEMS_ON_PAGE = 10;
+
     /**
+     * @var PaginatorInterface
+     */
+    private $paginator;
+
+    /**
+     * @param PaginatorInterface $paginator
+     * @return HomeController
+     *
+     * @required
+     */
+    public function setPaginator(PaginatorInterface $paginator): self
+    {
+        $this->paginator = $paginator;
+
+        return $this;
+    }
+
+    /**
+     * @param Request $request
      * @return Response
      * @throws \LogicException
      *
      * @Route("/", name="home", methods={"GET", "HEAD"})
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $books = $this->getDoctrine()->getRepository(Book::class)->findAll();
+        $query = $this->getDoctrine()->getRepository(Book::class)->createQueryBuilder('self')->getQuery();
+        $page = $request->query->getInt('page', 1);
+        $books = $this->paginator->paginate($query, $page, self::ITEMS_ON_PAGE);
 
         return $this->render('home/index.html.twig', ['books' => $books]);
     }
