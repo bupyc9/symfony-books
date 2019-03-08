cb574a92c3f9034316557d9f698c36f000c7e36d
Pavel Afanasev <bupyc9@gmail.com>
2019-03-08 23:37:29 +0300

    Added book list api

---
+++ source/src/Controller/Api/BookController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\DTO\ResultDTO;
+use App\Entity\Book;
+use App\Pagination\PaginationFactory;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use FOS\RestBundle\Request\ParamFetcher;
+use FOS\RestBundle\View\View;
+
+class BookController extends AbstractFOSRestController
+{
+    public const ITEMS_ON_PAGE = 20;
+
+    /**
+     * @var PaginationFactory
+     */
+    private $paginationFactory;
+
+    /**
+     * @param PaginationFactory $paginationFactory
+     *
+     * @return BookController
+     *
+     * @required
+     */
+    public function setPaginationFactory(PaginationFactory $paginationFactory): self
+    {
+        $this->paginationFactory = $paginationFactory;
+
+        return $this;
+    }
+
+    /**
+     * @param ParamFetcher $paramFetcher
+     *
+     * @throws \Throwable
+     *
+     * @return View
+     *
+     * @Rest\Get("/books", name="api_books")
+     * @Rest\QueryParam(name="page", default="1", allowBlank=false, requirements="\d+")
+     * @Rest\QueryParam(name="count", default=BookController::ITEMS_ON_PAGE, allowBlank=false, requirements="\d+")
+     */
+    public function index(ParamFetcher $paramFetcher): View
+    {
+        $queryBuilder = $this->getDoctrine()->getRepository(Book::class)->createQueryBuilder('self');
+
+        $dto = new ResultDTO($this->paginationFactory->createCollection($paramFetcher, $queryBuilder, 'api_books'));
+
+        return $this->view($dto);
+    }
+}
