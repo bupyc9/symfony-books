666051b27311899a2c7d225dc6083e5d6035ac1f
Pavel Afanasev <bupyc9@gmail.com>
2019-03-18 00:11:11 +0300

    Added cache to Api\BookController

--- source/src/Controller/Api/BookController.php
+++ source/src/Controller/Api/BookController.php
@@ -7,17 +7,24 @@ namespace App\Controller\Api;
 use App\DTO\Input\StoreBookDTO;
 use App\DTO\ResultDTO;
 use App\DTO\SuccessDTO;
+use App\Entity\Author;
 use App\Entity\Book;
 use App\Exception\FormValidationException;
 use App\Form\StoreBookForm;
 use App\Pagination\PaginationFactory;
+use App\Service\Cache;
+use Doctrine\ORM\NonUniqueResultException;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcher;
 use FOS\RestBundle\View\View;
-use LogicException;
+use Psr\Cache\CacheException;
+use Psr\Cache\InvalidArgumentException;
+use Symfony\Component\Cache\Adapter\TagAwareAdapterInterface;
 use Symfony\Component\Form\Exception\AlreadySubmittedException;
+use Symfony\Component\Form\Exception\LogicException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class BookController extends AbstractFOSRestController
 {
@@ -27,6 +34,11 @@ class BookController extends AbstractFOSRestController
      * @var PaginationFactory
      */
     private $paginationFactory;
+
+    /**
+     * @var TagAwareAdapterInterface
+     */
+    private $cache;
 
     /**
      * @param PaginationFactory $paginationFactory
@@ -43,9 +55,26 @@ class BookController extends AbstractFOSRestController
     }
 
     /**
+     * @param TagAwareAdapterInterface $cache
+     *
+     * @return BookController
+     *
+     * @required
+     */
+    public function setCache(TagAwareAdapterInterface $cache): self
+    {
+        $this->cache = $cache;
+
+        return $this;
+    }
+
+    /**
      * @param ParamFetcher $paramFetcher
      *
-     * @throws \Throwable
+     * @throws CacheException
+     * @throws InvalidArgumentException
+     * @throws \InvalidArgumentException
+     * @throws \LogicException
      *
      * @return View
      *
@@ -55,23 +84,64 @@ class BookController extends AbstractFOSRestController
      */
     public function index(ParamFetcher $paramFetcher): View
     {
-        $queryBuilder = $this->getDoctrine()->getRepository(Book::class)->createQueryBuilder('self');
+        $cacheKey = Cache::createKey(__METHOD__);
+        $item = $this->cache->getItem($cacheKey);
 
-        $dto = new ResultDTO($this->paginationFactory->createCollection($paramFetcher, $queryBuilder, 'api_books'));
+        if (!$item->isHit()) {
+            $queryBuilder = $this->getDoctrine()->getRepository(Book::class)->createQueryBuilder('self')
+                ->join('self.author', 'author')
+                ->addSelect('author')
+                ->addOrderBy('self.createdAt', 'ASC')
+            ;
+
+            $dto = new ResultDTO($this->paginationFactory->createCollection($paramFetcher, $queryBuilder, 'api_books'));
+
+            $item->set($dto);
+            $item->tag([Cache::createKey(Book::class), Cache::createKey(Author::class)]);
+            $this->cache->save($item);
+        }
+
+        $dto = $item->get();
 
         return $this->view($dto);
     }
 
     /**
-     * @param Book $book
+     * @param int $id
+     *
+     * @throws CacheException
+     * @throws InvalidArgumentException
+     * @throws NonUniqueResultException
+     * @throws \LogicException
      *
      * @return View
      *
      * @Rest\Get("/books/{id<\d+>}", name="api_book_show")
      */
-    public function show(Book $book): View
+    public function show(int $id): View
     {
-        $dto = new ResultDTO($book);
+        $cacheKey = Cache::createKey(__METHOD__.$id);
+        $item = $this->cache->getItem($cacheKey);
+
+        if (!$item->isHit()) {
+            $book = $this->getDoctrine()->getRepository(Book::class)->createQueryBuilder('self')
+                ->join('self.author', 'author')
+                ->addSelect('author')
+                ->where('self.id = :id')
+                ->setParameter('id', $id)
+                ->getQuery()
+                ->getOneOrNullResult()
+            ;
+            if (null === $book) {
+                throw new NotFoundHttpException('Book not found');
+            }
+
+            $dto = new ResultDTO($book);
+            $item->set($dto);
+            $item->tag([Cache::createKey(Author::class)]);
+            $this->cache->save($item);
+        }
+        $dto = $item->get();
 
         return $this->view($dto);
     }
@@ -79,7 +149,8 @@ class BookController extends AbstractFOSRestController
     /**
      * @param Book $book
      *
-     * @throws LogicException
+     * @throws InvalidArgumentException
+     * @throws \LogicException
      *
      * @return View
      *
@@ -93,6 +164,8 @@ class BookController extends AbstractFOSRestController
 
         $dto = new ResultDTO(new SuccessDTO());
 
+        $this->cache->invalidateTags([Cache::createKey(Book::class), Cache::createKey(Author::class)]);
+
         return $this->view($dto);
     }
 
@@ -100,6 +173,8 @@ class BookController extends AbstractFOSRestController
      * @param Request $request
      *
      * @throws AlreadySubmittedException
+     * @throws InvalidArgumentException
+     * @throws \LogicException
      * @throws LogicException
      *
      * @return View
@@ -130,6 +205,8 @@ class BookController extends AbstractFOSRestController
 
         $result = new ResultDTO($book);
 
+        $this->cache->invalidateTags([Cache::createKey(Book::class), Cache::createKey(Author::class)]);
+
         return $this->view($result);
     }
 
@@ -138,6 +215,8 @@ class BookController extends AbstractFOSRestController
      * @param Request $request
      *
      * @throws AlreadySubmittedException
+     * @throws InvalidArgumentException
+     * @throws \LogicException
      * @throws LogicException
      *
      * @return View
@@ -166,6 +245,8 @@ class BookController extends AbstractFOSRestController
         $em->flush();
 
         $result = new ResultDTO($book);
+
+        $this->cache->invalidateTags([Cache::createKey(Book::class), Cache::createKey(Author::class)]);
 
         return $this->view($result);
     }
