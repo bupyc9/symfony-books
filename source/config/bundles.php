c87a0dc7cfee89b27d7c87712a91763f133f0306
Pavel Afanasev <bupyc9@gmail.com>
2019-04-18 21:20:17 +0300

    Added library 'nelmio/api-doc-bundle' via composer

--- source/config/bundles.php
+++ source/config/bundles.php
@@ -20,4 +20,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
